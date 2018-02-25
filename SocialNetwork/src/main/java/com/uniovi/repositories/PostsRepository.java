798593ad8d68284f5b2194bf3f099490194226d9
Javier Díez García <javi_codema@hotmail.com>
2018-02-25 19:13:53 +0100

    Remove fail directory

--- SocialNetwork/src/main/java/com/uniovi/repositories/PostsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uniovi.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.uniovi.entities.Post;
-
-public interface PostsRepository extends CrudRepository<Post, Long> {
-}
-
