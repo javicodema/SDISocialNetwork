e2bbafacccf81affc270041f6f07332399c095d2
dromir <uo250757@uniovi.es>
2018-03-15 09:38:10 +0100

    You can now send friendrequests, and the logout button has been added when a user is authenticated

--- src/main/java/com/uniovi/controllers/FriendshipsController.java
+++ src/main/java/com/uniovi/controllers/FriendshipsController.java
@@ -1,18 +1,35 @@
 package com.uniovi.controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.uniovi.entities.FriendshipRequest;
+import com.uniovi.entities.User;
+import com.uniovi.services.FriendshipsService;
+import com.uniovi.services.UsersService;
+
 @Controller
 public class FriendshipsController {
+	@Autowired
+	private UsersService usersService;
+	@Autowired
+	private FriendshipsService frService;
+
 	@RequestMapping("/requests/received")
 	public String getList() {
 		return "requests/received";
 	}
 
 	@RequestMapping("/requests/add/{id}")
-	public String setRequest(@PathVariable Long id) {
+	public String setRequest(@PathVariable Long id, Principal principal) {
+		User receiver = usersService.getUser(id);
+		User sender = usersService.getUserByEmail(principal.getName());
+		FriendshipRequest fr = new FriendshipRequest(sender, receiver);
+		frService.addFriendship(fr);
 		return "redirect:/user/list";
 	}
 }
--- src/main/java/com/uniovi/services/FriendshipsService.java
+++ src/main/java/com/uniovi/services/FriendshipsService.java
@@ -1,12 +1,17 @@
 package com.uniovi.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.uniovi.entities.FriendshipRequest;
+import com.uniovi.repositories.FriendshipsRepository;
 
 @Service
 public class FriendshipsService {
-	public void addFriendship(FriendshipRequest fr) {
+	@Autowired
+	FriendshipsRepository frRepository;
 
+	public void addFriendship(FriendshipRequest fr) {
+		frRepository.save(fr);
 	}
 }
